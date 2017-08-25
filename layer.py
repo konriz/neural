650e4ba259459a71c69a0f60d1f7eabcbd7734a8
konriz <konriz@gmail.com>
2017-08-25 15:25:36 +0200

    end of work
    added competitive learning layer

--- layer.py
+++ layer.py
@@ -8,3 +8,14 @@ class Layer:
             neb = Neuron(inputs)
             neb.new_name(i)
             self.grid.append(neb)
+
+    def competitive_learning(self, learning_matrix):
+        for i in range(len(learning_matrix)):
+            learning_vector = learning_matrix[i]
+            results = []
+            for neuron in self.grid:
+                results.append(neuron.run(learning_vector))
+            max_index = results.index(max(results))
+            best = self.grid[max_index]
+            best.learn_anti([learning_vector])
+        return
--- main.py
+++ main.py
@@ -2,29 +2,28 @@ from neuron import Neuron
 from tools import signal_gen
 from layer import Layer
 
-layer = Layer(30, 3)
-training_signal = signal_gen(100)
+layer = Layer(100, 3)
+w0 = []
 for neuron in layer.grid:
-    neuron.learn_anti(training_signal)
+    w0.append(neuron.weights)
+training_signal = signal_gen(10000)
+layer.competitive_learning(training_signal)
+
+for neuron in layer.grid:
+#    neuron.learn_anti(training_signal)
     print "\nMy name is %s" % neuron.name
+    learn_matrix = []
+    for wei in range(len(neuron.weights)):
+        learn_matrix.append(neuron.weights[wei] - w0[layer.grid.index(neuron)][wei])
+    print "I've learned: %s" % learn_matrix
 #    print "My weights are  %s " % neuron.weights
     run_vectors = [[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0], [0, 0, 1], [1, 0, 1], [0, 1, 1], [1, 1, 1]]
     reactions = []
-    index = 0
-    max_reaction = -1
-    max_index = -1
-    min_reaction = 1
-    min_index = -1
+
     for vector in run_vectors:
-        reaction = neuron.run(vector)
-        reactions.append(reaction)
-        if reaction > max_reaction:
-            max_reaction = reaction
-            max_index = index
-        if reaction < min_reaction:
-            min_reaction = reaction
-            min_index = index
-        index += 1
+        reactions.append(neuron.run(vector))
+
+    min_max = [max(reactions), reactions.index(max(reactions)), min(reactions), reactions.index(min(reactions))]
 #    print "My reactions are: %s" % reactions
-    print "My best reaction is: %s for vector %s" % (max_reaction, run_vectors[max_index])
-    print "My worse reaction is: %s for vector %s" % (min_reaction, run_vectors[min_index])
+#    print "My best reaction is: %s for vector %s" % (min_max[0], run_vectors[min_max[1]])
+#    print "My worse reaction is: %s for vector %s" % (min_max[2], run_vectors[min_max[3]])
